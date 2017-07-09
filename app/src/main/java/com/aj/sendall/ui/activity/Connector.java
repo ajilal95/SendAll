44706a668703e311a5548c8becbd2d320d9fa7bc
ajilal <ajilal95@gmail.com>
2017-07-09 21:31:50 +0530

    09/07/17 1

--- app/src/main/java/com/aj/sendall/ui/activity/Connector.java
+++ app/src/main/java/com/aj/sendall/ui/activity/Connector.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 import com.aj.sendall.R;
 import com.aj.sendall.application.AndroidApplication;
@@ -18,6 +21,11 @@ import javax.inject.Inject;
 
 public class Connector extends AppCompatActivity {
     private RecyclerView availableConns;
+    private LinearLayout transBgLayout;
+    private ProgressBar pBarLoadingConns;
+    private LinearLayout buttonLayout;
+    private ImageView imgBtnInitConn;
+    private ImageView imgBtnScanConn;
     @Inject
     LocalWifiManager localWifiManager;
 
@@ -31,16 +39,62 @@ public class Connector extends AppCompatActivity {
         ((AndroidApplication)getApplication()).getDaggerInjector().inject(this);
         findViews();
         initViews();
+        setListeners();
     }
 
     private void findViews(){
-        availableConns = (RecyclerView) findViewById(R.id.recyclr_vw_available_conns);
+        availableConns = (RecyclerView) findViewById(R.id.recyclr_vw_connector_available_conns);
+        transBgLayout = (LinearLayout) findViewById(R.id.lyt_connector_trans_bg);
+        pBarLoadingConns = (ProgressBar) findViewById(R.id.prg_bar_connector_loading_conns);
+        buttonLayout = (LinearLayout) findViewById(R.id.lyt_connector_create_connection_buttons);
+        imgBtnInitConn = (ImageView) findViewById(R.id.img_btn_connector_initiate_connection);
+        imgBtnScanConn = (ImageView) findViewById(R.id.img_btn_connector_search_connection);
     }
 
     private void initViews(){
         availableConns.setLayoutManager(new LinearLayoutManager(this));
         connectorAdapter = new ConnectorAdapter(this, new ClickListenerForConnectorItem());
         availableConns.setAdapter(connectorAdapter);
+
+        pBarLoadingConns.setAlpha(0);
+    }
+
+    private void setListeners(){
+        imgBtnInitConn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animateViewsOnButtonClicked(v);
+            }
+        });
+
+        imgBtnScanConn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animateViewsOnButtonClicked(v);
+            }
+        });
+    }
+
+    private void animateViewsOnButtonClicked(View clickedButton){
+        if(clickedButton.equals(imgBtnInitConn)){
+            imgBtnInitConn.animate().setDuration(500).translationX(imgBtnInitConn.getWidth()).withEndAction(new OnButtonClickPrimaryAnimationFinished());
+            imgBtnScanConn.animate().setDuration(500).translationX(imgBtnScanConn.getWidth());
+        } else {
+            imgBtnScanConn.animate().setDuration(500).translationX(-imgBtnScanConn.getWidth()).withEndAction(new OnButtonClickPrimaryAnimationFinished());
+            imgBtnInitConn.animate().setDuration(500).translationX(-imgBtnInitConn.getWidth());
+        }
+    }
+
+    private class OnButtonClickPrimaryAnimationFinished implements Runnable{
+        @Override
+        public void run() {
+            buttonClickTransitionFinished();
+        }
+    }
+
+    private void buttonClickTransitionFinished() {
+        buttonLayout.animate().alpha(0).translationY(buttonLayout.getHeight()).setDuration(500);
+        pBarLoadingConns.animate().alpha(1).translationY(buttonLayout.getHeight()).setDuration(500);
     }
 
     @Override
